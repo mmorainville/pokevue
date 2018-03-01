460fe43fdf2b47a55889f5eb723b94aaeec335fa
Mathieu Morainville <mathieumorainville@hotmail.com>
2018-03-01 01:50:17 +0100

    WIP: feat: use a Subject to pause Observables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,14 +15,29 @@
   import Grid from '../components/Grid'
   import Rx from 'rxjs/Rx'
 
+  const pauser = new Rx.Subject()
+
   let keyDowns = Rx.Observable.fromEvent(window, 'keydown')
+  // keyDowns.connect()
+
   let keyUps = Rx.Observable.fromEvent(window, 'keyup')
 
-  let keyPresses = keyDowns
-    .merge(keyUps)
-    .groupBy(e => e.keyCode)
-    // .map(group => group.distinctUntilChanged(null, e => e.type))
-    .mergeAll()
+  // let disposableKeyDowns = keyDowns.connect()
+
+  // pauser
+  //   .switchMap(paused => paused ? Rx.Observable.never() : keyDowns)
+  //   .subscribe(x => console.log(x))
+
+  // pauser
+  //   .switchMap(active =>
+  //     Rx.Observable.if(() => active, keyDowns, Rx.Observable.empty())
+  //   ).subscribe()
+
+  // let keyPresses = keyDowns
+  //   .merge(keyUps)
+  //   .groupBy(e => e.keyCode)
+  //   // .map(group => group.distinctUntilChanged(null, e => e.type))
+  //   .mergeAll()
 
   let timerWalk
   let oldXPos
@@ -38,12 +53,48 @@
     mounted () {
       // window.addEventListener('keydown', this.onKeyDown, false)
       // window.addEventListener('keyup', this.onKeyUp, false)
-      this.$observables.keyPresses$.subscribe((e) => {
-        if (e.type === 'keydown') {
+      // this.$observables.keyPresses$.subscribe((e) => {
+      //   if (e.type === 'keydown') {
+      //     this.onKeyDown(e)
+      //   } else {
+      //     this.onKeyUp(e)
+      //   }
+      // })
+      // this.$observables.pauser$.subscribe((e) => {
+      //   console.log('KEYDOWNS', e)
+      //   // this.onKeyDown(e)
+      // })
+
+      // pauser
+      //   .switchMap(paused => paused ? Rx.Observable.never() : keyDowns)
+      //   .subscribe(e => {
+      //     this.onKeyUp(e)
+      //     console.log(e)
+      //   })
+
+      this.$observables.pauser$.switchMap(paused => paused ? Rx.Observable.never() : keyDowns)
+        .subscribe(e => {
           this.onKeyDown(e)
-        } else {
-          this.onKeyUp(e)
-        }
+          console.log(e)
+        })
+
+      pauser.next(false)
+
+      // pauser.next(true)
+      // setTimeout(() => {
+      //   pauser.next(false)
+      //
+      //   setTimeout(() => {
+      //     pauser.next(true)
+      //   }, 5000)
+      // }, 2000)
+
+      // setTimeout(() => {
+      //   pauser.next(false)
+      // }, 5000)
+
+      this.$observables.keyUps$.subscribe((e) => {
+        this.onKeyUp(e)
       })
     },
     data () {
@@ -67,7 +118,10 @@
     },
     subscriptions() {
       return {
-        keyPresses$: keyPresses
+        // keyPresses$: keyPresses,
+        keyDowns$: keyDowns,
+        keyUps$: keyUps,
+        pauser$: pauser
       }
     },
     computed: {
@@ -79,10 +133,8 @@
     },
     methods: {
       onKeyDown (e) {
-        let previousPosition = {
-          x: this.xPos,
-          y: this.yPos
-        }
+        console.log('onKeyDown')
+        pauser.next(true)
 
         if (Object.keys(this.pressedKeys).includes('' + e.keyCode)) {
           this.pressedKeys[e.keyCode] = true
@@ -106,6 +158,7 @@
                 if (!this.pressedKeys[e.keyCode] && (this.yPos - oldYPos) % 16 === 0) {
                   clearInterval(timerWalk)
                   this.isWalking = false
+                  pauser.next(false)
                 }
               }, 12)
             }
@@ -124,6 +177,7 @@
                 if (!this.pressedKeys[e.keyCode] && (this.yPos - oldYPos) % 16 === 0) {
                   clearInterval(timerWalk)
                   this.isWalking = false
+                  pauser.next(false)
                 }
               }, 12)
             }
@@ -146,6 +200,7 @@
                 if (!this.pressedKeys[e.keyCode] && (this.xPos - oldXPos) % 16 === 0) {
                   clearInterval(timerWalk)
                   this.isWalking = false
+                  pauser.next(false)
                 }
               }, 12)
             }
@@ -164,6 +219,7 @@
                 if (!this.pressedKeys[e.keyCode] && (this.xPos - oldXPos) % 16 === 0) {
                   clearInterval(timerWalk)
                   this.isWalking = false
+                  pauser.next(false)
                 }
               }, 12)
             }
@@ -174,7 +230,7 @@
         }
       },
       onKeyUp (e) {
-        // console.log('onKeyUp')
+        console.log('onKeyUp')
 
         if (Object.keys(this.pressedKeys).includes('' + e.keyCode)) {
           this.pressedKeys[e.keyCode] = false
