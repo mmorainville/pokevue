fc4896481c33a7cca59f6e85c92bc094be4c86d5
Mathieu Morainville <mathieumorainville@hotmail.com>
2019-06-22 01:01:37 +0200

    WIP: feat: add the map

--- README.md
+++ README.md
@@ -27,3 +27,6 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Resources
+- [Creating a game-size world map of Pokémon Fire Red](https://medium.com/@mmmulani/creating-a-game-size-world-map-of-pok%C3%A9mon-fire-red-614da729476a)
--- src/game/index.js
+++ src/game/index.js
@@ -26,29 +26,16 @@ let game = new Phaser.Game(config)
 console.log(game)
 
 function preload () {
-  this.load.setBaseURL('http://labs.phaser.io')
-
-  this.load.image('sky', 'assets/skies/space3.png')
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-  this.load.image('red', 'assets/particles/red.png')
+  this.load.image('sky', '../assets/map/spz3zUx_small.png')
+  this.load.image('logo', 'http://labs.phaser.io/assets/sprites/phaser3-logo.png')
 }
 
 function create () {
   this.add.image(this.scale.height / 2, this.scale.width / 2, 'sky')
-
-  let particles = this.add.particles('red')
-
-  let emitter = particles.createEmitter({
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: 'ADD'
-  })
 
   let logo = this.physics.add.image(400, 100, 'logo')
 
   logo.setVelocity(100, 200)
   logo.setBounce(1, 1)
   logo.setCollideWorldBounds(true)
-
-  emitter.startFollow(logo)
 }
