602871f5fcaaff4d54edc6fa60bb5faa4864a043
bropad <pierre.a.deville@gmail.com>
2019-07-01 00:53:36 +0200

    WIP: feat: add Pokedex

--- src/components/interface/menu/pokedex.vue
+++ src/components/interface/menu/pokedex.vue
@@ -1,13 +1,41 @@
 <template>
   <v-container fluid grid-list-xl text-xs-center>
-    <v-layout row wrap>
-      <v-flex xs2 offset-xs10>
-        <v-card>
-          <v-list>
-            <v-list-tile v-for="(pokemon, index) in pokemons" :key="index">
-              <v-list-tile-title>{{ pokemon.name }}</v-list-tile-title>
-            </v-list-tile>
-          </v-list>
+    <v-layout align-center justify-center row fill-height>
+      <v-flex xs6 >
+        <v-card style="height: 300px">
+          <v-layout align-center justify-center row fill-height>
+            <v-flex xs4 >
+              <v-list class="list-pokemon">
+                <v-list-tile v-for="(pokemon, index) in pokemons" :key="index">
+                  <v-list-tile-title @click="selectedPokemon = pokemon">{{ pokemon.name }}</v-list-tile-title>
+                </v-list-tile>
+              </v-list>
+            </v-flex>
+            <v-flex xs8 >
+              <v-layout>
+                <v-flex xs5>
+                  <v-img
+                    :src="selectedPokemon.image"
+                    height="125px"
+                    contain
+                  ></v-img>
+                </v-flex>
+                <v-flex xs7>
+                  <div primary-title>
+                    <div>
+                      <div class="headline">{{ selectedPokemon.name }}</div>
+                      <div>{{ selectedPokemon.type }}</div>
+                      <div>n° {{ selectedPokemon.index }}</div>
+                    </div>
+                  </div>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+              <div class="pa-3">
+                {{ selectedPokemon.description }}
+              </div>
+            </v-flex>
+          </v-layout>
         </v-card>
       </v-flex>
     </v-layout>
@@ -19,21 +47,96 @@ export default {
   name: 'Pokedex',
   data () {
     return {
-      pokemons: []
+      pokemons: [],
+      selectedPokemon: {}
     }
   },
   mounted () {
     this.pokemons = this.getPokedex()
+    this.selectedPokemon = this.pokemons[0]
   },
   methods: {
     getPokedex: () => {
       return [
         {
+          index: 112,
           name: 'Rhinoféros',
-          type: 'sol - roche'
+          type: 'sol - roche',
+          image: 'https://www.pokepedia.fr/images/8/80/Rhinof%C3%A9ros-RFVF.png',
+          description: 'La corne de Rhinoféros peut même casser du diamant brut. Et avec un simple coup de queue, il peut détruire un bâtiment. La peau de ce Pokémon est incroyablement dure. Un boulet de canon ne lui ferait même pas une égratignure.'
+        },
+        {
+          index: 1,
+          name: 'Bulbizarre',
+          type: 'plante - poison',
+          image: 'https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png',
+          description: 'Bulbizarre passe son temps à faire la sieste sous le soleil. Il y a une graine sur son dos. Il absorbe les rayons du soleil pour faire doucement pousser la graine.'
+        },
+        {
+          index: 1,
+          name: 'Bulbizarre',
+          type: 'plante - poison',
+          image: 'https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png',
+          description: 'Bulbizarre passe son temps à faire la sieste sous le soleil. Il y a une graine sur son dos. Il absorbe les rayons du soleil pour faire doucement pousser la graine.'
+        },
+        {
+          index: 1,
+          name: 'Bulbizarre',
+          type: 'plante - poison',
+          image: 'https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png',
+          description: 'Bulbizarre passe son temps à faire la sieste sous le soleil. Il y a une graine sur son dos. Il absorbe les rayons du soleil pour faire doucement pousser la graine.'
+        },
+        {
+          index: 1,
+          name: 'Bulbizarre',
+          type: 'plante - poison',
+          image: 'https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png',
+          description: 'Bulbizarre passe son temps à faire la sieste sous le soleil. Il y a une graine sur son dos. Il absorbe les rayons du soleil pour faire doucement pousser la graine.'
+        },
+        {
+          index: 1,
+          name: 'Bulbizarre',
+          type: 'plante - poison',
+          image: 'https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png',
+          description: 'Bulbizarre passe son temps à faire la sieste sous le soleil. Il y a une graine sur son dos. Il absorbe les rayons du soleil pour faire doucement pousser la graine.'
+        },
+        {
+          index: 1,
+          name: 'Bulbizarre',
+          type: 'plante - poison',
+          image: 'https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png',
+          description: 'Bulbizarre passe son temps à faire la sieste sous le soleil. Il y a une graine sur son dos. Il absorbe les rayons du soleil pour faire doucement pousser la graine.'
+        },
+        {
+          index: 1,
+          name: 'Bulbizarre',
+          type: 'plante - poison',
+          image: 'https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png',
+          description: 'Bulbizarre passe son temps à faire la sieste sous le soleil. Il y a une graine sur son dos. Il absorbe les rayons du soleil pour faire doucement pousser la graine.'
+        },
+        {
+          index: 1,
+          name: 'Bulbizarre',
+          type: 'plante - poison',
+          image: 'https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png',
+          description: 'Bulbizarre passe son temps à faire la sieste sous le soleil. Il y a une graine sur son dos. Il absorbe les rayons du soleil pour faire doucement pousser la graine.'
+        },
+        {
+          index: 1,
+          name: 'Bulbizarre',
+          type: 'plante - poison',
+          image: 'https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png',
+          description: 'Bulbizarre passe son temps à faire la sieste sous le soleil. Il y a une graine sur son dos. Il absorbe les rayons du soleil pour faire doucement pousser la graine.'
         }
       ]
     }
   }
 }
 </script>
+
+<style>
+  .list-pokemon {
+    height: 300px;
+    overflow-y: auto;
+  }
+</style>
--- src/views/Interface.vue
+++ src/views/Interface.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
     <Menu v-if="showMenu"/>
+    <Pokedex v-if="showMenu"/>
     <Snackbar/>
   </div>
 </template>
 
 <script>
 import Menu from '@/components/interface/menu'
+import Pokedex from '@/components/interface/menu/pokedex'
 import Snackbar from '@/components/interface/Snackbar'
 
 export default {
   name: 'Interface',
-  components: { Menu, Snackbar },
+  components: { Menu, Pokedex, Snackbar },
   data () {
     return {
       showMenu: false
