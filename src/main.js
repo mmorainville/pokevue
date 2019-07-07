faeb7a5330edd39193b7e79acbcf3b08ab3591ff
bropad <pierre.a.deville@gmail.com>
2019-07-07 18:38:19 +0200

    feat: add Axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import './plugins/vuetify'
+import './plugins/axios'
 import './shared/app-bus'
 import './shared/app-snackbar'
 
---
+++ src/plugins/axios.js
@@ -0,0 +1,4 @@
+import axios from 'axios'
+import Vue from 'vue'
+
+Vue.prototype.$http = axios
