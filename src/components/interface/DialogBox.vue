dae4af5c01fb59cbb14acaf14f7103589e60eacb
Mathieu Morainville <mathieumorainville@hotmail.com>
2019-07-15 17:45:32 +0200

    WIP: feat: add TypeIt to the dialog box

--- src/components/interface/DialogBox.vue
+++ src/components/interface/DialogBox.vue
@@ -12,21 +12,23 @@
       <v-subheader>Prof. Chen</v-subheader>
       <v-list-tile avatar>
         <v-list-tile-content>
-            Hello, there! Glad to meet you! Welcome to the world of Pokémon!
-            My name is Oak. People affectionately refer to me as the Pokémon Professor. This world…
-            …is inhabited far and wide by creatures called Pokémon!
-            For some people, Pokémon are pets. Other use them for battling. As for myself…
-            I study Pokémon as a profession. But first, tell me a little about yourself.
-            Now tell me. Are you a boy? Or are you a girl?
-            Let's begin with your name. What is it? Right…
-            So your name is Red.
-            This is my grandson.
-            He's been your rival since you both were babies.
-            …Erm, what was his name now? …Er, was it Blue?
-            That's right! I remember now! His name is Blue!
-            Red! Your own very Pokémon legend is about to unfold!
-            A world of dreams and adventures with Pokémon awaits!
-            Let's go!
+          <span class="dialog"></span>
+
+          <!--Hello, there! Glad to meet you! Welcome to the world of Pokémon!
+          My name is Oak. People affectionately refer to me as the Pokémon Professor. This world…
+          …is inhabited far and wide by creatures called Pokémon!
+          For some people, Pokémon are pets. Other use them for battling. As for myself…
+          I study Pokémon as a profession. But first, tell me a little about yourself.
+          Now tell me. Are you a boy? Or are you a girl?
+          Let's begin with your name. What is it? Right…
+          So your name is Red.
+          This is my grandson.
+          He's been your rival since you both were babies.
+          …Erm, what was his name now? …Er, was it Blue?
+          That's right! I remember now! His name is Blue!
+          Red! Your own very Pokémon legend is about to unfold!
+          A world of dreams and adventures with Pokémon awaits!
+          Let's go!-->
         </v-list-tile-content>
         <v-list-tile-action>
           <v-icon color="grey lighten-1">play_arrow</v-icon>
@@ -37,6 +39,10 @@
 </template>
 
 <script>
+import TypeIt from 'typeit'
+
+let typeIt = null
+
 export default {
   name: 'DialogBox',
   data () {
@@ -47,13 +53,50 @@ export default {
   created () {
     this.$appBus.$on('dialog:open', () => {
       // console.log('Open dialog', this.isVisible)
-      this.isVisible = true
+      if (this.isVisible) {
+        // Continue reading
+        if (typeIt) {
+          typeIt.unfreeze()
+        }
+      } else {
+        this.isVisible = true
+        this.startReading()
+      }
     })
 
     this.$appBus.$on('dialog:close', () => {
       // console.log('Close dialog')
       this.isVisible = false
+      if (typeIt) {
+        typeIt.reset()
+        typeIt = null
+      }
     })
+  },
+  mounted () {
+
+  },
+  methods: {
+    startReading () {
+      let options = {
+        strings: [
+          'Hello, there! Glad to meet you! Welcome to the world of Pokémon!',
+          'My name is Oak. People affectionately refer to me as the Pokémon Professor.'
+        ],
+        speed: 20,
+        cursor: false,
+        afterString: (step, queue, instance) => {
+          console.log(step, queue, instance)
+          if (typeIt) {
+            typeIt.freeze()
+          }
+        },
+        breakLines: false
+      }
+
+      // eslint-disable-next-line no-new
+      typeIt = new TypeIt('.dialog', options).go()
+    }
   }
 }
 </script>
